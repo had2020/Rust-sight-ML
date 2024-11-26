bdda4dbc5f6da5198c5eba36383c3004e3e805b3
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-11-26 12:47:00 -0800

    Update main.rs

--- main/src/main.rs
+++ main/src/main.rs
@@ -5,13 +5,17 @@ use candle_core::{Device, Result, Tensor};
 struct Model {
     first: Tensor,
     second: Tensor,
+    third: Tensor,
 }
 
 impl Model {
     fn forward(&self, image: &Tensor) -> Result<Tensor> {
         let x = image.matmul(&self.first)?;
         let x = x.relu()?;
-        x.matmul(&self.second)
+        let x = x.matmul(&self.second)?;
+        let x = x.relu()?;
+        let x = x.matmul(&self.third)?;
+        Ok(x)
     }
 }
 
@@ -26,16 +30,17 @@ fn main() -> Result<()> {
     };
     let device = Device::Metal(metal_device);
 
-
-    let first = Tensor::randn(0f32, 1.0, (784, 100), &device)?;
-    let second = Tensor::randn(0f32, 1.0, (100, 10), &device)?;
-    let model = Model { first, second };
+    // 3D tensor let first = Tensor::randn(0f32, 1.0, (10, 10, 10), &device)?;
+    let first = Tensor::randn(0f32, 1.0, (784, 100), &device)?; // frist two random, tensor with shape 784, 100
+    let second = Tensor::randn(0f32, 1.0, (100, 10), &device)?; // the second number must match the first number in the under row
+    let third = Tensor::randn(0f32, 1.0, (10, 10), &device)?;
+    let model = Model { first, second, third };
 
     let dummy_image = Tensor::randn(0f32, 1.0, (1, 784), &device)?;
 
     let digit = model.forward(&dummy_image)?;
 
-    
+
     println!("Digit {digit:?} digit");
     Ok(())
 }
